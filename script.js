363b89e99497ce84818cff05d2f722ecc01319c8
AJHA96 <117764226+AJHA96@users.noreply.github.com>
2023-05-31 00:03:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function threeSum(n) {
+let bin=0;
+	let i=1;
+	while(n>1){
+		let rem=n%2;
+		bin +=rem*i;
+		i*=10;
+		n/=2;
+	}
+  console.log(bin);
 }
 
 module.exports = threeSum;
